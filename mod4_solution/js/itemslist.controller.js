9e0ada277223968a0edc2ec27c353626d4d6e962
khayamuddin31 <khayamuddin31@gmail.com>
2016-10-15 16:27:09 +0530

    Update itemslist.controller.js

--- mod4_solution/js/itemslist.controller.js
+++ mod4_solution/js/itemslist.controller.js
@@ -8,6 +8,7 @@ angular.module('MenuApp')
 ItemsListController.$inject = ['items']
 function ItemsListController(items) {
   var itemsCtrl = this;
+  console.log('Items found', items);
   itemsCtrl.items = items.menu_items;
   itemsCtrl.category = items.category;
 }
