938f362bda3dc1538066f5fc6c6324552eb48355
khayamuddin31 <khayamuddin31@gmail.com>
2016-10-15 16:53:38 +0530

    Update itemslist.controller.js

--- mod4_solution/js/itemslist.controller.js
+++ mod4_solution/js/itemslist.controller.js
@@ -1,16 +1,16 @@
 (function () {
 'use strict';
 
-angular.module('MenuApp')
-.controller('ItemsListController ', ItemsListController );
+  angular.module('MenuApp')
+  .controller('ItemsListController ', ItemsListController );
 
 
-ItemsListController.$inject = ['items']
-function ItemsListController(items) {
-  var itemsCtrl = this;
-  console.log('Items found', items);
-  //itemsCtrl.items = items.menu_items;
-  //itemsCtrl.category = items.category;
-}
+  ItemsListController.$inject = ['items'];
+  function ItemsListController(items) {
+    var itemsCtrl = this;
+    console.log('Items found', items);
+    //itemsCtrl.items = items.menu_items;
+    //itemsCtrl.category = items.category;
+  }
 
 })();
